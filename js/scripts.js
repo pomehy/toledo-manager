19ac236a508a85fef45d48363553faf752f4cf15
pomehy <hlopkov.gleb@mail.ru>
2020-05-13 18:10:47 +0300

    add js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', function(e) {
+    var map = document.querySelector('#map-wrap iframe')
+    if(e.target.id === 'map-wrap') {
+      map.style.pointerEvents = 'all'
+    } else {
+      map.style.pointerEvents = 'none'
+    }
+  })
+
+ 
+$(document).ready(function () {
+      $('.main-nav a[href^="#"], *[data-href^="#"]').on('click', function (e) {
+          e.preventDefault();
+          var t = 1000;
+          var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+          $('html,body').stop().animate({
+              scrollTop: $(d).offset().top
+          }, t);
+      });
+});
+
